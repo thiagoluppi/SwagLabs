9444527416539d6cb90bb2c3f665f4321b8ae724
thiagoluppi <thiago.luppi@icloud.com>
2023-10-02 12:32:04 +0100

    First Commit - Going to Swag Labs login page

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+require('dotenv').config()
 
 /**
  * @see https://playwright.dev/docs/test-configuration
@@ -29,6 +29,10 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
+    baseURL: process.env.BASE_URL,
+    screenshot:'only-on-failure',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -38,15 +42,15 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
